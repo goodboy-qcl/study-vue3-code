4796d19cf48cc8d0129db6ee757be5009a8eaab8
qiucanlei <772904865@qq.com>
2023-03-15 17:07:05 +0800

    feat: effect 调用测试实现

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -15,4 +15,20 @@ describe('effect', () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
+
+  // effect 测试
+  it('should return runner when call effect', () => {
+    // 1. effect(fn) 执行的时候 需要返回一个function函数（称之为 runner）
+    // 2. 调用这个 runner 的时候回去执行 effect 内部接收到的 fn
+    // 3. 执行完这个 fn 之后， 他会把 fn 的返回值 return 出去
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return 'foo'
+    })
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe('foo');
+  });
 })
