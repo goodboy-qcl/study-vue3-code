aa38340bb363fef38025c5335e7f2dfa6cb980f7
qiucanlei <772904865@qq.com>
2023-03-16 17:03:02 +0800

    feat: 实现 onStop 测试

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -82,4 +82,23 @@ describe('effect', () => {
     runner();
     expect(dummy).toBe(3);
   });
+
+  // onStop 测试
+  it('onStop', () => {
+    const obj = reactive({
+      foo: 1
+    });
+    const onStop = jest.fn();
+    let dummy;
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      {
+        onStop
+      }
+    )
+    stop(runner);
+    expect(onStop).toHaveBeenCalledTimes(1);
+  });
 })
