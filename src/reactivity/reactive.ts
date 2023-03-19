f8e07311f85f67ae03cd8dd026e826ebb6609362
邱灿磊 <772904865@qq.com>
2023-03-19 16:53:34 +0800

    feat: 实现 readonly 功能 并且抽离getter

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,16 +1,29 @@
 import { track, trigger } from "./effect";
 
+// 抽离getter
+function createGetter (isReadonly = false) {
+  return function get (target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  }
+}
+
 export function reactive (raw) {
   return new Proxy(raw, {
     // target: 当前的对象， key：用户访问的key
-    get (target, key) {
-      const res = Reflect.get(target, key);
+    // get (target, key) {
+    //   const res = Reflect.get(target, key);
 
-      // 依赖收集
-      track(target, key);
-      // 返回访问的对象
-      return res;
-    },
+    //   // 依赖收集
+    //   track(target, key);
+    //   // 返回访问的对象
+    //   return res;
+    // },
+    // 使用抽离的get
+    get: createGetter(),
     // target: 当前的对象， key：用户访问的key, value: 设置的值
     set (target, key, value) {
       const res = Reflect.set(target, key, value);
@@ -19,5 +32,20 @@ export function reactive (raw) {
       // 返回设置的新对象
       return res;
     },
+  })
+}
+
+// readonly 
+export function readonly (raw) {
+  return new Proxy(raw, {
+    // get (target, key) {
+    //   const res = Reflect.get(target, key);
+
+    //   return res;
+    // },
+    get: createGetter(true),
+    set (target, key, value) {
+      return true;
+    }
   })
 }
